d83b44098b9426cddc0d26eba155fdacbe1674ac
Hobr <mkowes@vip.qq.com>
2019-08-14 11:53:04 +0800

    忙于假期作业 过两天继续

--- src/renderer/components/Page/PageFooter.vue
+++ src/renderer/components/Page/PageFooter.vue
@@ -1,8 +1,17 @@
 <template>
-  <h1>test</h1>
+  <el-row>
+    <el-button class="alt" type="primary" round icon="el-icon-suitcase" @click="open('https://github.com/Hobr/samp-launcher-electron')">Powered by Hobr/samp-launcher-electron.</el-button>
+  </el-row>
 </template>
 
 <script>
+export default {
+  methods: {
+    open (link) {
+      this.$electron.shell.openExternal(link)
+    }
+  }
+}
 </script>
 
 <style>
--- src/renderer/components/Page/PageHeader.vue
+++ src/renderer/components/Page/PageHeader.vue
@@ -1,9 +1,21 @@
 <template>
-  <img id="logo" src="~@/assets/logo.png" alt="logo">
+  <el-row :gutter="20">
+    <el-col :span="6">
+      <img id="logo" src="~@/assets/logo.png" alt="logo">
+    </el-col>
+    <el-col :span="18">
+      <img id="banner" src="~@/assets/banner.png" alt="banner">
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 </script>
 
-<style>
+<style>  
+#logo #banner
+{
+  height: auto;
+  width: auto;
+}
 </style>
