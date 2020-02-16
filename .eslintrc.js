9b043fa559f04451e26d8af6d43f3a4569dd5ad9
unlsycn <g.humphrey.sln@gmail.com>
2020-02-16 16:23:48 +0800

    配置了eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,30 @@ module.exports = {
         browser: true,
         node: true
     },
+    rules: {
+        "quotes": [2, "double"],//双引号
+        "semi": [2, "always"], //语句强制分号结尾
+        "indent": [2, 4], //缩进风格
+        "brace-style": [2, "allman"],//allman(大括号换行)风格
+        "camelcase": [2, { properties: "never" }],
+        "array-bracket-spacing": [2, "never"],
+        "default-case": 2,
+        "curly": [2, "all"],
+        "dot-location": [2, "property"],
+        "eol-last": 2,
+        "eqeqeq": [2, "allow-null"],
+        "max-depth": [2, 8],
+        "max-lines": [2, 500],
+        "max-params": [2, { "max": 8 }],
+        "no-alert": 1,
+        "no-var": 2,
+        "semi-spacing": 2,
+        "semi-style": [2, "last"]
+    },
     extends: "standard",
     globals: {
-        __static: true
-    },
-    plugins: ["html"],
-    rules: {
-        quotes: [0, "single"],
-        semi: [2, "always"], //语句强制分号结尾
-        indent: [2, 4], //缩进风格
+        __static: true,
+        plugins: ["html"],
         // allow paren-less arrow functions
         "arrow-parens": 0,
         // allow async-await
